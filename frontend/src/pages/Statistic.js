fc9de28f6d77278105ce1687894721b9b3870c2e
Mikhail Topchilo <miksoft.tm@gmail.com>
2021-04-14 12:58:51 +0500

    feat: Add statistic range calendar

--- frontend/src/pages/Statistic.js
+++ frontend/src/pages/Statistic.js
@@ -2,8 +2,9 @@ import _ from 'lodash'
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Container, Dimmer, Loader, Grid, Button } from 'semantic-ui-react'
+import { Container, Dimmer, Loader, Grid, Button, Icon } from 'semantic-ui-react'
 
+import DateRangePicker from '@wojtekmaj/react-daterange-picker'
 import MainContainer from '../components/MainContainer'
 import FullStats from '../components/FullStats'
 
@@ -15,35 +16,53 @@ class Statistic extends Component {
 
     state = {
         loader: false,
+        rangeStart: moment().subtract(1,'d').format('YYYY-MM-DD'),
+        rangeEnd: moment().format('YYYY-MM-DD')
     }
 
     componentDidMount() {
         const { dispatch, storeStatistic } = this.props
+        const { rangeStart, rangeEnd } = this.state
 
         let last_update = (! _.isEmpty(storeStatistic) ? moment().unix() - storeStatistic.update : null)
 
         if (last_update === null || (last_update < -180 || last_update > 180))
-            dispatch(meteoActions.fetchDataStatistic())
+            dispatch(meteoActions.fetchDataStatistic(rangeStart, rangeEnd))
     }
 
     changePeriod = ( period ) => {
         const { dispatch } = this.props
 
         if ( period !== this.state.period ) {
-            this.setState({ loader: true, period })
+            let rangeStart = moment().subtract(period,'d').format('YYYY-MM-DD'),
+                rangeEnd   = moment().format('YYYY-MM-DD')
 
-            let date_start = moment().subtract(period,'d').format('YYYY-MM-DD'),
-                date_end   = moment().format('YYYY-MM-DD')
+            this.setState({ loader: true, period, rangeStart, rangeEnd })
 
-            dispatch(meteoActions.fetchDataStatistic(date_start, date_end)).then(() => {
+            dispatch(meteoActions.fetchDataStatistic(rangeStart, rangeEnd)).then(() => {
                 this.setState({ loader: false })
             })
         }
     }
 
+    handleDatePicker = range => {
+        const { dispatch } = this.props
+
+        this.setState({ loader: true })
+
+        let rangeStart = moment(range[0]).format('YYYY-MM-DD'),
+            rangeEnd = moment(range[1]).format('YYYY-MM-DD')
+
+        this.setState({rangeStart, rangeEnd})
+
+        dispatch(meteoActions.fetchDataStatistic(rangeStart, rangeEnd)).then(() => {
+            this.setState({ loader: false })
+        })
+    }
+
     render() {
         const { storeStatistic } = this.props
-        const { loader } = this.state
+        const { loader, rangeStart, rangeEnd } = this.state
 
         return (
             <MainContainer
@@ -52,11 +71,24 @@ class Statistic extends Component {
                 <Container>
                     <div className='toolBar'>
                         <Button.Group size='mini'>
-                            <Button color='grey' onClick={() => this.changePeriod(0)}>Сегодня</Button>
-                            <Button color='grey' onClick={() => this.changePeriod(1)}>Вчера</Button>
+                            <Button color='grey' onClick={() => this.changePeriod(1)}>Сутки</Button>
                             <Button color='grey' onClick={() => this.changePeriod(7)}>Неделя</Button>
                             <Button color='grey' onClick={() => this.changePeriod(30)}>Месяц</Button>
                         </Button.Group>
+                        <DateRangePicker
+                            className='range-calendar'
+                            onChange={(v) => {this.handleDatePicker(v)}}
+                            calendarIcon={<Icon name='calendar alternate outline' size='large' />}
+                            minDate={new Date('2020-04-10')}
+                            maxDate={new Date()}
+                            locale='ru-RU'
+                            clearIcon={null}
+                            format='dd.MM.yyyy'
+                            value={[
+                                rangeStart,
+                                rangeEnd
+                            ]}
+                        />
                     </div>
                     { (! _.isEmpty(storeStatistic) && ! loader) ? (
                         <FullStats
