92e2d1e07cba490117e6ca07eea60af1ebc9ee64
Mik <miksoft.tm@gmail.com>
2021-08-22 21:18:49 +0500

    feat: Toolbar with datepicker and range picker to individual component

--- frontend/src/layouts/Toolbar.js
+++ frontend/src/layouts/Toolbar.js
@@ -1,21 +1,22 @@
 import React from 'react'
-
-import { Button, Icon } from "semantic-ui-react";
-import DateRangePicker from "@wojtekmaj/react-daterange-picker";
+import { Button, Icon } from 'semantic-ui-react'
+import DateRangePicker from '@wojtekmaj/react-daterange-picker'
 
 const Toolbar = props => {
-    const { data } = props
+    const { rangeStart, rangeEnd } = props
+    const handleChangePeriod = period => props.changePeriod(period)
+    const handleDatePicker = range => props.changeData(range)
 
     return (
         <div className='toolBar'>
             <Button.Group size='mini'>
-                <Button color='grey' onClick={() => this.changePeriod(1)}>Сутки</Button>
-                <Button color='grey' onClick={() => this.changePeriod(7)}>Неделя</Button>
-                <Button color='grey' onClick={() => this.changePeriod(30)}>Месяц</Button>
+                <Button color='grey' onClick={() => handleChangePeriod(1)}>Сутки</Button>
+                <Button color='grey' onClick={() => handleChangePeriod(7)}>Неделя</Button>
+                <Button color='grey' onClick={() => handleChangePeriod(30)}>Месяц</Button>
             </Button.Group>
             <DateRangePicker
                 className='range-calendar'
-                onChange={(v) => {this.handleDatePicker(v)}}
+                onChange={(range) => {handleDatePicker(range)}}
                 calendarIcon={<Icon name='calendar alternate outline' size='large' />}
                 minDate={new Date('2020-04-10')}
                 maxDate={new Date()}
--- frontend/src/pages/Statistic.js
+++ frontend/src/pages/Statistic.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Container, Button, Icon, Message } from 'semantic-ui-react'
+import { Container, Message } from 'semantic-ui-react'
 import { getUrlParameter } from '../data/functions' // createWindRose
 
-import DateRangePicker from '@wojtekmaj/react-daterange-picker'
 import MainContainer from '../components/MainContainer'
 
 import Chart from '../layouts/Chart'
+import Toolbar from '../layouts/Toolbar';
 
 import temphumdwind from '../charts/temphumdwind'
 import luxpress from '../charts/luxpress'
@@ -112,34 +112,12 @@ class Statistic extends Component {
                 title='Статистика'
             >
                 <Container>
-                    <div className='toolBar'>
-                        <Button.Group size='mini'>
-                            <Button color='grey' onClick={() => this.changePeriod(1)}>Сутки</Button>
-                            <Button color='grey' onClick={() => this.changePeriod(7)}>Неделя</Button>
-                            <Button color='grey' onClick={() => this.changePeriod(30)}>Месяц</Button>
-                        </Button.Group>
-                        <DateRangePicker
-                            className='range-calendar'
-                            onChange={(v) => {this.handleDatePicker(v)}}
-                            calendarIcon={<Icon name='calendar alternate outline' size='large' />}
-                            minDate={new Date('2020-04-10')}
-                            maxDate={new Date()}
-                            locale='ru-RU'
-                            clearIcon={null}
-                            format='dd.MM.yyyy'
-                            value={[
-                                rangeStart._d,
-                                rangeEnd._d
-                            ]}
-                        />&nbsp;
-                        <Button
-                            size='mini'
-                            icon='download'
-                            color='grey'
-                            as='a'
-                            href={`https://api.miksoft.pro/meteo/get/csv/?date_start=${rangeStart.format('YYYY-MM-DD')}&date_end=${rangeEnd.format('YYYY-MM-DD')}`}
-                        />
-                    </div>
+                    <Toolbar
+                        changePeriod={(period) => this.changePeriod(period)}
+                        changeData={(range) => this.handleDatePicker(range)}
+                        rangeStart={rangeStart}
+                        rangeEnd={rangeEnd}
+                    />
                     {(storeStatistic.update === false) && (
                         <Message negative>
                             <Message.Header>Данные не найдены</Message.Header>
