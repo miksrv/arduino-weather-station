0dae9840b19078d9cafc5843f2b80ccb2c35066c
Mik <miksoft.tm@gmail.com>
2021-09-24 22:40:31 +0500

    feat: Update some packets and dependencies to max versions

--- frontend/src/pages/Forecast.js
+++ frontend/src/pages/Forecast.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Table, Dimmer, Loader, Container, Image, Popup, Responsive } from 'semantic-ui-react'
+import { Table, Dimmer, Loader, Image, Popup } from 'semantic-ui-react'
 import { valueColor } from '../data/functions'
 
 import MainContainer from '../components/MainContainer'
-import ForeacstTile from '../layouts/ForeacstTile'
+// import ForeacstTile from '../layouts/ForeacstTile'
 
 import moment from 'moment'
 
@@ -31,66 +31,57 @@ class Forecast extends Component {
                 onUpdateData={this.updateWeatherData}
             >
                 { ! _.isEmpty(storeForecast) ? (
-                    <div>
-                        <Responsive as={Container} minWidth={768}>
-                            <Table celled inverted selectable className='weather-table'>
-                                <Table.Header>
-                                    <Table.Row>
-                                        <Table.HeaderCell></Table.HeaderCell>
-                                        <Table.HeaderCell>Дата, время</Table.HeaderCell>
-                                        <Table.HeaderCell>Описание</Table.HeaderCell>
-                                        <Table.HeaderCell>
-                                            <Popup content='Температура воздуха' trigger={<span>T (℃)</span>} />
-                                        </Table.HeaderCell>
-                                        <Table.HeaderCell>
-                                            <Popup content='Эффективная температура - характеристика комфортности с учётом влажности воздуха и скорости ветра: температура, которую должен иметь сухой воздух при штиле, чтобы теплоощущение человека было таким же, как в данном влажном воздухе при наличии ветра.' trigger={<span>Te (℃)</span>} />
-                                        </Table.HeaderCell>
-                                        <Table.HeaderCell>
-                                            <Popup content='Влажность воздуха' trigger={<span>H (%)</span>} />
-                                        </Table.HeaderCell>
-                                        <Table.HeaderCell>
-                                            <Popup content='Давление (мм. рт. ст.)' trigger={<span>P (мм)</span>} />
-                                        </Table.HeaderCell>
-                                        <Table.HeaderCell colSpan='2'>Ветер (м\с)</Table.HeaderCell>
-                                    </Table.Row>
-                                </Table.Header>
-                                <Table.Body>
-                                    {storeForecast.data.map((item, key) => (
-                                        <Table.Row key={key}>
-                                            <Table.Cell>
-                                                <Image src={'https://openweathermap.org/img/wn/' + item.weather[0].icon + '.png'} />
-                                            </Table.Cell>
-                                            <Table.Cell>{moment.unix(item.dt).format("DD.MM.Y H:mm")}</Table.Cell>
-                                            <Table.Cell>{item.weather[0].description}</Table.Cell>
-                                            <Table.Cell>
-                                                {valueColor(Number((item.main.temp).toFixed(1)))}
-                                            </Table.Cell>
-                                            <Table.Cell>
-                                                {valueColor(Number((item.main.feels_like).toFixed(1)))}
-                                            </Table.Cell>
-                                            <Table.Cell>
-                                                {item.main.humidity}
-                                            </Table.Cell>
-                                            <Table.Cell>
-                                                {Number((item.main.pressure * 0.75).toFixed(1))}
-                                            </Table.Cell>
-                                            <Table.Cell>
-                                                {Number((item.wind.speed).toFixed(1))}
-                                            </Table.Cell>
-                                            <Table.Cell>
-                                                {item.wind.deg}°
-                                            </Table.Cell>
-                                        </Table.Row>
-                                    ))}
-                                </Table.Body>
-                            </Table>
-                        </Responsive>
-                        <Responsive as={Container} maxWidth={766}>
-                            <ForeacstTile
-                                data={storeForecast.data}
-                            />
-                        </Responsive>
-                    </div>
+                    <Table celled inverted selectable className='weather-table'>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell></Table.HeaderCell>
+                                <Table.HeaderCell>Дата, время</Table.HeaderCell>
+                                <Table.HeaderCell>Описание</Table.HeaderCell>
+                                <Table.HeaderCell>
+                                    <Popup content='Температура воздуха' trigger={<span>T (℃)</span>} />
+                                </Table.HeaderCell>
+                                <Table.HeaderCell>
+                                    <Popup content='Эффективная температура - характеристика комфортности с учётом влажности воздуха и скорости ветра: температура, которую должен иметь сухой воздух при штиле, чтобы теплоощущение человека было таким же, как в данном влажном воздухе при наличии ветра.' trigger={<span>Te (℃)</span>} />
+                                </Table.HeaderCell>
+                                <Table.HeaderCell>
+                                    <Popup content='Влажность воздуха' trigger={<span>H (%)</span>} />
+                                </Table.HeaderCell>
+                                <Table.HeaderCell>
+                                    <Popup content='Давление (мм. рт. ст.)' trigger={<span>P (мм)</span>} />
+                                </Table.HeaderCell>
+                                <Table.HeaderCell colSpan='2'>Ветер (м\с)</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {storeForecast.data.map((item, key) => (
+                                <Table.Row key={key}>
+                                    <Table.Cell>
+                                        <Image src={'https://openweathermap.org/img/wn/' + item.weather[0].icon + '.png'} />
+                                    </Table.Cell>
+                                    <Table.Cell>{moment.unix(item.dt).format("DD.MM.Y H:mm")}</Table.Cell>
+                                    <Table.Cell>{item.weather[0].description}</Table.Cell>
+                                    <Table.Cell>
+                                        {valueColor(Number((item.main.temp).toFixed(1)))}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {valueColor(Number((item.main.feels_like).toFixed(1)))}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {item.main.humidity}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {Number((item.main.pressure * 0.75).toFixed(1))}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {Number((item.wind.speed).toFixed(1))}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {item.wind.deg}°
+                                    </Table.Cell>
+                                </Table.Row>
+                            ))}
+                        </Table.Body>
+                    </Table>
                 ) : (
                     <Dimmer active>
                         <Loader>Загрузка</Loader>
