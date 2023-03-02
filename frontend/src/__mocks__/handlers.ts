f194418fe3e90c5c2eb300a02135d2d889bab708
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-03-02 10:00:51 -0800

    Update handlers.ts

--- frontend/src/__mocks__/handlers.ts
+++ frontend/src/__mocks__/handlers.ts
@@ -4,7 +4,7 @@ import currentData from './rest/current'
 import forecastData from './rest/forecast'
 import sensorsData from './rest/sensors'
 
-const randomVal = Math.random();
+const randomVal = Math.random()
 
 export const handlers = [
     rest.get('**/get/current', (req, res, ctx) => {
