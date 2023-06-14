706a28c17247473bbf1b2f3692b157d4b24134fd
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-06-14 10:02:17 -0700

    Update footer.test.tsx

--- frontend/src/__tests__/footer.test.tsx
+++ frontend/src/__tests__/footer.test.tsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-import packageInfo from '../../package.json'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { update } from 'update'
@@ -11,6 +10,8 @@ import { store } from 'app/store'
 import translate from 'functions/translate'
 
 import Footer from 'components/footer'
+
+import packageInfo from '../../package.json'
 
 describe('Test Footer component', () => {
     const language = translate()
