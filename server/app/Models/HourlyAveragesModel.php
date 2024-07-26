da47f9fd3834d58ae19a32984100e40bec2fdc1a
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-07-25 18:44:29 -0700

    Implemented API DB models

---
+++ server/app/Models/HourlyAveragesModel.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class HourlyAveragesModel extends Model
+{
+    protected $table          = 'hourly_averages';
+    protected $primaryKey     = 'id';
+    protected $useTimestamps  = false;
+    protected $returnType     = 'array';
+    protected $allowedFields  = [
+        'hour',
+        'average_temperature',
+        'average_feels_like',
+        'average_pressure',
+        'average_humidity',
+        'average_dew_point',
+        'average_uvi',
+        'average_clouds',
+        'average_visibility',
+        'average_wind_speed',
+        'average_wind_deg',
+        'average_wind_gust',
+        'weather_id',
+        'weather_main',
+        'weather_description',
+        'weather_icon'
+    ];
+
+    protected $validationRules = [
+        'hour'                => 'required|valid_date',
+        'average_temperature' => 'permit_empty|decimal',
+        'average_feels_like'  => 'permit_empty|decimal',
+        'average_pressure'    => 'permit_empty|decimal',
+        'average_humidity'    => 'permit_empty|decimal',
+        'average_dew_point'   => 'permit_empty|decimal',
+        'average_uvi'         => 'permit_empty|decimal',
+        'average_clouds'      => 'permit_empty|decimal',
+        'average_visibility'  => 'permit_empty|decimal',
+        'average_wind_speed'  => 'permit_empty|decimal',
+        'average_wind_deg'    => 'permit_empty|decimal',
+        'average_wind_gust'   => 'permit_empty|decimal',
+        'weather_id'          => 'permit_empty|integer',
+        'weather_main'        => 'permit_empty|max_length[50]',
+        'weather_description' => 'permit_empty|max_length[100]',
+        'weather_icon'        => 'permit_empty|max_length[10]',
+    ];
+
+    protected $validationMessages = [
+
+    ];
+
+    protected $casts = [
+        'hour'                => 'datetime',
+        'average_temperature' => '?float',
+        'average_feels_like'  => '?float',
+        'average_pressure'    => '?float',
+        'average_humidity'    => '?float',
+        'average_dew_point'   => '?float',
+        'average_uvi'         => '?float',
+        'average_clouds'      => '?float',
+        'average_visibility'  => '?float',
+        'average_wind_speed'  => '?float',
+        'average_wind_deg'    => '?float',
+        'average_wind_gust'   => '?float',
+        'weather_id'          => '?int',
+        'weather_main'        => '?string',
+        'weather_description' => '?string',
+        'weather_icon'        => '?string',
+    ];
+}
---
+++ server/app/Models/RawWeatherDataModel.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class RawWeatherDataModel extends Model
+{
+    protected $table         = 'raw_weather_data';
+    protected $primaryKey    = 'id';
+    protected $useTimestamps = false;
+    protected $returnType    = 'array';
+    protected $allowedFields = [
+        'timestamp',
+        'source',
+        'sunrise',
+        'sunset',
+        'temperature',
+        'feels_like',
+        'pressure',
+        'humidity',
+        'dew_point',
+        'uvi',
+        'clouds',
+        'visibility',
+        'wind_speed',
+        'wind_deg',
+        'wind_gust',
+        'weather_id',
+        'weather_main',
+        'weather_description',
+        'weather_icon'
+    ];
+
+    protected $validationRules = [
+        'timestamp'           => 'required|valid_date',
+        'source'              => 'required|in_list[OpenWeatherMap,CustomStation,OtherSource]',
+        'sunrise'             => 'permit_empty|valid_date',
+        'sunset'              => 'permit_empty|valid_date',
+        'temperature'         => 'permit_empty|decimal',
+        'feels_like'          => 'permit_empty|decimal',
+        'pressure'            => 'permit_empty|integer',
+        'humidity'            => 'permit_empty|integer',
+        'dew_point'           => 'permit_empty|decimal',
+        'uvi'                 => 'permit_empty|decimal',
+        'clouds'              => 'permit_empty|integer',
+        'visibility'          => 'permit_empty|integer',
+        'wind_speed'          => 'permit_empty|decimal',
+        'wind_deg'            => 'permit_empty|integer',
+        'wind_gust'           => 'permit_empty|decimal',
+        'weather_id'          => 'permit_empty|integer',
+        'weather_main'        => 'permit_empty|max_length[50]',
+        'weather_description' => 'permit_empty|max_length[100]',
+        'weather_icon'        => 'permit_empty|max_length[10]',
+    ];
+
+    protected $validationMessages = [
+        'source' => [
+            'in_list' => 'The source must be one of: OpenWeatherMap, CustomStation, OtherSource.',
+        ],
+    ];
+
+    protected $casts = [
+        'timestamp'           => 'datetime',
+        'source'              => 'string',
+        'sunrise'             => '?datetime',
+        'sunset'              => '?datetime',
+        'temperature'         => '?float',
+        'feels_like'          => '?float',
+        'pressure'            => '?int',
+        'humidity'            => '?int',
+        'dew_point'           => '?float',
+        'uvi'                 => '?float',
+        'clouds'              => '?int',
+        'visibility'          => '?int',
+        'wind_speed'          => '?float',
+        'wind_deg'            => '?int',
+        'wind_gust'           => '?float',
+        'weather_id'          => '?int',
+        'weather_main'        => '?string',
+        'weather_description' => '?string',
+        'weather_icon'        => '?string',
+    ];
+}
