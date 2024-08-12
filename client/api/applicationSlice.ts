d2ec428e47a6efcd68069032a9fb975a8455e66f
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-08-12 11:15:10 -0700

    Refactoring UI application slice (Redux RTK)

--- client/api/applicationSlice.ts
+++ client/api/applicationSlice.ts
@@ -1,17 +1,11 @@
-import { setCookie } from 'cookies-next'
-
 import i18Config from '../next-i18next.config'
 
-import { ApiTypes } from '@/api/types'
 import { LOCAL_STORAGE } from '@/tools/constants'
 import * as LocalStorage from '@/tools/localstorage'
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 type ApplicationStateProps = {
-    showOverlay?: boolean
-    showAuthDialog?: boolean
-    userLocation?: ApiTypes.LatLonCoordinate
-    locale?: ApiTypes.LocaleType
+    locale?: 'ru' | 'en'
 }
 
 export const getStorageLocale = (): string | undefined =>
@@ -21,34 +15,16 @@ export const getStorageLocale = (): string | undefined =>
 
 const applicationSlice = createSlice({
     initialState: {
-        locale: getStorageLocale(),
-        showAuthDialog: false,
-        showOverlay: false
+        locale: getStorageLocale()
     } as ApplicationStateProps,
     name: 'application',
     reducers: {
-        closeAuthDialog: (state) => {
-            state.showOverlay = false
-            state.showAuthDialog = false
-        },
-        openAuthDialog: (state) => {
-            state.showOverlay = true
-            state.showAuthDialog = true
-        },
-        setLocale: (state, { payload }: PayloadAction<ApiTypes.LocaleType>) => {
+        setLocale: (state, { payload }: PayloadAction<'ru' | 'en'>) => {
             state.locale = payload
-        },
-        setUserLocation: (state, { payload }: PayloadAction<ApiTypes.LatLonCoordinate>) => {
-            setCookie(LOCAL_STORAGE.LOCATION, `${payload.lat};${payload.lon}`)
-
-            state.userLocation = payload
-        },
-        toggleOverlay: (state, { payload }: PayloadAction<boolean>) => {
-            state.showOverlay = payload
         }
     }
 })
 
-export const { toggleOverlay, closeAuthDialog, openAuthDialog, setLocale, setUserLocation } = applicationSlice.actions
+export const { setLocale } = applicationSlice.actions
 
 export default applicationSlice.reducer
