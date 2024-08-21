2c2122a6c71aae7b0797327e7ab799c7fecc778f
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-08-21 13:44:27 -0700

    Refactoring UI WidgetForecastTable component

--- client/components/widget-forecast-table/WidgetForecastTable.tsx
+++ client/components/widget-forecast-table/WidgetForecastTable.tsx
@@ -3,30 +3,21 @@ import React from 'react'
 import styles from './styles.module.sass'
 
 import { ApiModel } from '@/api'
-import WeatherIcon from '@/components/weather-icon'
 import Table, { Column } from '@/ui/table'
 
 interface WidgetProps {
     data?: ApiModel.Weather[]
+    columns?: Column<ApiModel.Weather>[]
     loading?: boolean
 }
 
-const WidgetForecastTable: React.FC<WidgetProps> = ({ data, loading }) => {
-    const columns: Column<ApiModel.Weather>[] = [
-        { header: 'Date', accessor: 'date', className: styles.cellDate, isSortable: true },
-        { header: 'Weather', accessor: 'weatherId', className: styles.cellIcon, formatter: (weatherId) => <WeatherIcon weatherId={weatherId as number} /> },
-        { header: 'Temp', accessor: 'temperature', className: styles.cellTemperature, isSortable: true },
-        { header: 'Clouds', accessor: 'clouds', className: styles.cellClouds, isSortable: true }
-    ]
-
-    return (
-        <div className={styles.widgetForecastTable}>
-            <Table<ApiModel.Weather>
-                columns={columns}
-                data={data}
-            />
-        </div>
-    )
-}
+const WidgetForecastTable: React.FC<WidgetProps> = ({ data, columns, loading }) =>(
+    <div className={styles.widgetForecastTable}>
+        <Table<ApiModel.Weather>
+            columns={columns}
+            data={data}
+        />
+    </div>
+)
 
 export default WidgetForecastTable
