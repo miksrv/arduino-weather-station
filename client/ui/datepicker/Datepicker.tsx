1212d5e446b1ed31fa3aef4a5eeda9d46bb7f239
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-09-06 11:14:34 -0700

    Implemented DatePicker preset active functions

--- client/ui/datepicker/Datepicker.tsx
+++ client/ui/datepicker/Datepicker.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import dayjs from 'dayjs'
+
+import styles from './styles.module.sass'
 
 import Calendar, { CalendarProps } from '@/ui/datepicker/Calendar'
 
@@ -19,52 +21,44 @@ export type CalendarPresetType = {
     key: PresetOption
     endDate: Date
 }
+const nowDate = dayjs.utc() // Для общего времени
 
-const getPresetDates = (preset: PresetOption): CalendarPresetType => {
-    const now = dayjs.utc()
-
-    switch (preset) {
-        case PresetOption.TODAY:
-            return { key: preset, endDate: now.toDate() }
-
-        case PresetOption.DAY:
-            return { key: preset, endDate: now.subtract(1, 'day').toDate() }
-
-        case PresetOption.WEEK:
-            return { key: preset, endDate: now.subtract(1, 'week').toDate() }
-
-        case PresetOption.MONTH:
-            return { key: preset, endDate: now.subtract(1, 'month').toDate() }
-
-        case PresetOption.QUARTER:
-            return { key: preset, endDate: now.subtract(3, 'month').toDate() }
-
-        case PresetOption.HALF_YEAR:
-            return { key: preset, endDate: now.subtract(6, 'month').toDate() }
-
-        case PresetOption.YEAR:
-            return { key: preset, endDate: now.subtract(1, 'year').toDate() }
-
-        default:
-            return { key: PresetOption.TODAY, endDate: now.toDate() }
-    }
-}
+const timePresets: CalendarPresetType[] = [
+    { key: PresetOption.TODAY, endDate: nowDate.toDate() }, // Сегодняшний день
+    { key: PresetOption.DAY, endDate: nowDate.subtract(1, 'day').toDate() }, // Сутки назад
+    { key: PresetOption.WEEK, endDate: nowDate.subtract(1, 'week').toDate() }, // Неделя назад
+    { key: PresetOption.MONTH, endDate: nowDate.subtract(1, 'month').toDate() }, // Месяц назад
+    { key: PresetOption.QUARTER, endDate: nowDate.subtract(3, 'month').toDate() }, // Квартал назад
+    { key: PresetOption.HALF_YEAR, endDate: nowDate.subtract(6, 'month').toDate() }, // Полгода назад
+    { key: PresetOption.YEAR, endDate: nowDate.subtract(1, 'year').toDate() } // Год назад
+]
 
 const DatePicker: React.FC<DatePickerProps> = (props) => {
     const handlePresetSelect = (preset: PresetOption) => {
-        const { endDate } = getPresetDates(preset)
+        const endDate = timePresets?.find(({key}) => key === preset)?.endDate
         props?.onPeriodSelect?.(dayjs(endDate).format('YYYY-MM-DD'), dayjs.utc().format('YYYY-MM-DD')) // Вызов с начала и конца периода
     }
+
+    const findCurrentPreset = useCallback((key: PresetOption): CalendarPresetType | undefined => {
+        const preset = timePresets?.find((preset) => preset.key === key)
+
+        if (nowDate.isSame(props.endDate, 'day') && dayjs(props.startDate).isSame(preset?.endDate, 'day')) {
+            return preset
+        }
+
+        return undefined
+    }, [props.startDate, props.endDate])
 
     return (
         <>
             <div>
-                {Object.values(PresetOption).map((preset) => (
+                {timePresets?.map(({key}) => (
                     <button
-                        key={preset}
-                        onClick={() => handlePresetSelect(preset)}
+                        key={key}
+                        className={findCurrentPreset(key) && styles.presetActive}
+                        onClick={() => handlePresetSelect(key)}
                     >
-                        {preset}
+                        {key}
                     </button>
                 ))}
             </div>
