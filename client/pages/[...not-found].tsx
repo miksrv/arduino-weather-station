57a612ddba7acf8949ce327082c26d3891da5c85
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-12-27 20:47:29 -0800

    Implemented Not Found Page

---
+++ client/pages/[...not-found].tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { NextSeo } from 'next-seo'
+
+import AppLayout from '@/components/app-layout'
+import Menu from '@/components/app-layout/Menu'
+
+export default function CatchAllPage() {
+    const { i18n, t } = useTranslation('common')
+
+    return (
+        <AppLayout>
+            <NextSeo
+                title={t('page-not-found')}
+                description={t('page-not-found-description')}
+                canonical={process.env.NEXT_PUBLIC_SITE_LINK}
+                openGraph={{
+                    locale: i18n.language === 'ru' ? 'ru_RU' : 'en_US',
+                    siteName: t('weather-in-orenburg'),
+                    title: t('page-not-found'),
+                    type: 'website'
+                }}
+            />
+            <div className={'not-found'}>
+                <h1>{t('page-not-found')}</h1>
+                <div>{t('page-not-found-description')}</div>
+                <div className={'not-found-menu'}>
+                    <Menu />
+                </div>
+            </div>
+        </AppLayout>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const locale = context.locale || 'en'
+
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ['common'])),
+            notFound: true
+        }
+    }
+}
